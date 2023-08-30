59c37a6e4c2b5964d2750f493ec5c7afd936bd7a
Juan Carlos García <77403023+garcia-jc@users.noreply.github.com>
2023-08-29 19:04:39 -0600

    feat: ejemplo con WASI

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/garcia-jc/wasi-examples
 
 go 1.21.0
+
+require github.com/stealthrocket/net v0.2.1
---
+++ http/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/stealthrocket/net/http"
+	"github.com/stealthrocket/net/wasip1"
+)
+
+func main() {
+	logRequest := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "hola, desde WASI!")
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", logRequest)
+	addr := ":3000"
+	listener, err := wasip1.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Listening and serving at", addr)
+	srv := http.Server{
+		Handler: mux,
+		Addr:    addr,
+	}
+	err = srv.Serve(listener)
+	log.Println("server shutdown", err)
+}
